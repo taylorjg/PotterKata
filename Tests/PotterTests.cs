902f42a119f060636cc69d188819fe613569832b
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2014-04-11 19:52:32 +0100

    Refactored to lookup discount in a table

--- Tests/PotterTests.cs
+++ Tests/PotterTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Tests
@@ -6,11 +7,13 @@ namespace Tests
     [TestFixture]
     internal class PotterTests
     {
+        private const double UnitBookPrice = 8d;
+
         [Test]
         public void NoBooksCostZero()
         {
             var price = CalculatePriceForBooks(string.Empty);
-            Assert.That(price, Is.EqualTo(0));
+            Assert.That(price, Is.EqualTo(0d));
         }
 
         [TestCase("A")]
@@ -21,62 +24,74 @@ namespace Tests
         public void ASingleBookIsPricedCorrectly(string books)
         {
             var price = CalculatePriceForBooks(books);
-            Assert.That(price, Is.EqualTo(8));
+            Assert.That(price, Is.EqualTo(UnitBookPrice));
         }
 
-        [TestCase("AA", 2 * 8)]
-        [TestCase("AAA", 3 * 8)]
-        [TestCase("AAAA", 4 * 8)]
-        [TestCase("AAAAA", 5 * 8)]
-        [TestCase("BB", 2 * 8)]
-        [TestCase("BBB", 3 * 8)]
-        [TestCase("BBBB", 4 * 8)]
-        [TestCase("BBBBB", 5 * 8)]
-        [TestCase("AAAAAA", 6 * 8)]
-        [TestCase("AAAAAAA", 7 * 8)]
-        [TestCase("AAAAAAAA", 8 * 8)]
-        [TestCase("AAAAAAAAA", 9 * 8)]
-        [TestCase("AAAAAAAAAA", 10 * 8)]
+        [TestCase("AA", 2 * UnitBookPrice)]
+        [TestCase("AAA", 3 * UnitBookPrice)]
+        [TestCase("AAAA", 4 * UnitBookPrice)]
+        [TestCase("AAAAA", 5 * UnitBookPrice)]
+        [TestCase("BB", 2 * UnitBookPrice)]
+        [TestCase("BBB", 3 * UnitBookPrice)]
+        [TestCase("BBBB", 4 * UnitBookPrice)]
+        [TestCase("BBBBB", 5 * UnitBookPrice)]
+        [TestCase("AAAAAA", 6 * UnitBookPrice)]
+        [TestCase("AAAAAAA", 7 * UnitBookPrice)]
+        [TestCase("AAAAAAAA", 8 * UnitBookPrice)]
+        [TestCase("AAAAAAAAA", 9 * UnitBookPrice)]
+        [TestCase("AAAAAAAAAA", 10 * UnitBookPrice)]
         public void MultipleBooksOfTheSameTypeArePricedCorrectly(string books, double expectedPrice)
         {
             var price = CalculatePriceForBooks(books);
             Assert.That(price, Is.EqualTo(expectedPrice));
         }
 
-        [TestCase("AB", 2 * 8 * 0.95)]
-        [TestCase("ABC", 3 * 8 * 0.90)]
-        [TestCase("ABCD", 4 * 8 * 0.80)]
-        [TestCase("ABCDE", 5 * 8 * 0.75)]
+        [TestCase("AB", 2 * UnitBookPrice * 0.95d)]
+        [TestCase("ABC", 3 * UnitBookPrice * 0.90d)]
+        [TestCase("CDE", 3 * UnitBookPrice * 0.90d)]
+        [TestCase("ACE", 3 * UnitBookPrice * 0.90d)]
+        [TestCase("ABCD", 4 * UnitBookPrice * 0.80d)]
+        [TestCase("BCDE", 4 * UnitBookPrice * 0.80d)]
+        [TestCase("ABCDE", 5 * UnitBookPrice * 0.75d)]
+        [TestCase("EDCBA", 5 * UnitBookPrice * 0.75d)]
         public void MultipleBooksOfDifferentTypesArePricedCorrectly(string books, double expectedPrice)
         {
             var price = CalculatePriceForBooks(books);
-            Assert.That(price, Is.EqualTo(expectedPrice));
+            Assert.That(price, Is.EqualTo(RoundUp(expectedPrice)));
         }
+
+        private static double RoundUp(double d)
+        {
+            return System.Math.Round(d, 2);
+        }
+
+        private readonly static IDictionary<int, int> NumDifferentItems2PercentDiscount = new Dictionary<int, int>
+            {
+                {0, 0},
+                {1, 0},
+                {2, 5},
+                {3, 10},
+                {4, 20},
+                {5, 25}
+            };
 
         private static double CalculatePriceForBooks(string books)
         {
             var numBooks = books.Length;
+            double? price = null;
 
             if (books.ToCharArray().Distinct().Count() == numBooks)
             {
-                switch (numBooks)
-                {
-                    case 0:
-                        return 0;
-                    case 1:
-                        return 1 * 8;
-                    case 2:
-                        return 2 * 8 * 0.95;
-                    case 3:
-                        return 3 * 8 * 0.90;
-                    case 4:
-                        return 4 * 8 * 0.80;
-                    case 5:
-                        return 5 * 8 * 0.75;
-                }
+                var percentDiscount = NumDifferentItems2PercentDiscount[numBooks];
+                price = (numBooks * UnitBookPrice) / 100 * (100 - percentDiscount);
             }
 
-            return numBooks * 8;
+            if (!price.HasValue)
+            {
+                price = numBooks * UnitBookPrice;
+            }
+
+            return RoundUp(price.Value);
         }
     }
 }
